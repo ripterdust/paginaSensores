c6f3a377bfda293e9158fa8427008241a1c51c20
ripterdust <bryantello2010@hotmail.com>
2020-09-16 23:40:35 -0600

    Agregados los componentes de navegacion y estudiantes; agregado Bootstrap y la barra de navegación

--- src/src/App.js
+++ src/src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { Navegacion } from './components/navigationBar/navegación';
+import './components/navigationBar/navegación';
+import Navegacion from './components/navigationBar/navegación';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  render(){
+    return(
+      <div>
+        <Navegacion></Navegacion>
+      </div>
+    );
+  };
 }
 
 export default App;
---
+++ src/src/components/navigationBar/navegación.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Navegacion extends React.Component{
+    render(){
+        return(
+            <nav className="navbar navbar-dark bg-dark">
+                <a className="navbar-brand" href="/">Navbar</a>
+            </nav>
+        );
+    };
+}
+
+export default Navegacion;
